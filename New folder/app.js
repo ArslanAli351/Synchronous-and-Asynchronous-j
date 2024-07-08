b4c4a6f10abd8c0e36b1fe114f8a091ffea7b82d
Arslan <muhammadarslanrajput12@gmail.com>
2024-07-08 12:25:04 +0500

    Synchronous and Asynchronous in javascript

---
+++ New folder/app.js
@@ -0,0 +1,26 @@
+
+
+// function SynchronousAsynchronous(){
+// console.log("Hello world");
+
+// setTimeout(() => {
+//     console.log("arslan")
+// }, 1000);
+
+// console.log("Hello Javascript")
+// }
+// SynchronousAsynchronous()
+
+
+
+
+
+
+function SynchronousAsynchronous(){
+console.log("Hello HTML");
+setTimeout(() => {
+console.log("Hello CSS")
+},0);
+console.log("Hello javascript")
+}
+SynchronousAsynchronous()
